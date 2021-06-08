e88f8e5a488374d4e9232d9753e6e7046adc7654
richard jarram <rjarram@me.com>
2021-06-08 14:18:11 -0700

    setup versatile ruby config for Docker.

--- init.sql
+++
@@ -1,3 +0,0 @@
--- used in docker compose file: /docker-entrypoint-initdb.d/init.sql.
--- fires once on each container creation; creates new user forephemeral db instance.
-CREATE USER cryptotickr_development SUPERUSER;
